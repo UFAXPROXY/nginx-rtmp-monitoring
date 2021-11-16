9fc121b4eaa73044a6df97bb3a76e718de55b9c0
UFAXPROXY <92266683+UFAXPROXY@users.noreply.github.com>
2021-11-17 01:41:50 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env sh
 
-OS=`uname`
-# $(replace_in_file pattern file)
-function replace_in_file() {
-    if [ "$OS" = 'Darwin' ]; then
-        # for MacOS
-        sed -i '' -e "$1" "$2"
-    else
-        # for Linux and Windows
-        sed -i'' -e "$1" "$2"
-    fi
-}
+# OS=`uname`
+# # $(replace_in_file pattern file)
+# function replace_in_file() {
+#     if [ "$OS" = 'Darwin' ]; then
+#         # for MacOS
+#         sed -i '' -e "$1" "$2"
+#     else
+#         # for Linux and Windows
+#         sed -i'' -e "$1" "$2"
+#     fi
+# }
 
 #find host ip address
-ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+#ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 
 #generate session key
-key="44e154a78486e639d263af5b23290a10649ec2f4d25d3063ec0fb29b1460ceea"
+#key="44e154a78486e639d263af5b23290a10649ec2f4d25d3063ec0fb29b1460ceea"
 
 #generate random password
-password="Ufax@123456"
+#password="Ufax@123456"
 
 #configure the dashboard
-$(replace_in_file 's,rtmp_server_url.*,rtmp_server_url\":'"\"http://$ip:8080/stat.xml"\"'\,,' "config.json")
-$(replace_in_file 's,rtmp_server_stream_url.*,rtmp_server_stream_url\":'"\"rtmp://$ip:1935/live/"\"'\,,' "config.json")
-$(replace_in_file 's,rtmp_server_control_url.*,rtmp_server_control_url\":'"\"http://$ip:8080/control"\"'\,,' "config.json")
-$(replace_in_file 's,hls_server_stream_url.*,hls_server_stream_url\":'"\"http://$ip:8080/live/"\"'\,,' "config.json")
-$(replace_in_file 's,session_secret_key.*,session_secret_key\":'"\"$key"\"'\,,' "config.json")
-$(replace_in_file 's,password.*,password\":'"\"$password"\"'\,,' "config.json")
+# $(replace_in_file 's,rtmp_server_url.*,rtmp_server_url\":'"\"http://$ip:8080/stat.xml"\"'\,,' "config.json")
+# $(replace_in_file 's,rtmp_server_stream_url.*,rtmp_server_stream_url\":'"\"rtmp://$ip:1935/live/"\"'\,,' "config.json")
+# $(replace_in_file 's,rtmp_server_control_url.*,rtmp_server_control_url\":'"\"http://$ip:8080/control"\"'\,,' "config.json")
+# $(replace_in_file 's,hls_server_stream_url.*,hls_server_stream_url\":'"\"http://$ip:8080/live/"\"'\,,' "config.json")
+# $(replace_in_file 's,session_secret_key.*,session_secret_key\":'"\"$key"\"'\,,' "config.json")
+# $(replace_in_file 's,password.*,password\":'"\"$password"\"'\,,' "config.json")
 
 #start the contrainers
-docker-compose up --build -d
+#docker-compose up --build -d
+
+docker build -t fstv_dashboard .
